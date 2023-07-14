c256ded3c2136b13f61585f98c04aca5185eca68
SySagar <sysagar07@gmail.com>
2023-07-14 20:57:51 +0530

    feat:styling

--- routes/messageQueue.js
+++ routes/messageQueue.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import subscribeMessage from '../controllers/subscriberController.js';
 import publishMessage from '../controllers/publisherController.js';
-import getAllMessages from '../controllers/getAllMessagesController.js';
+import getBasicMessage from '../controllers/getAllMessagesController.js';
 
 const router = express.Router()
 
-router.get("/",getAllMessages );
+router.get("/",getBasicMessage );
 router.post("/publish",publishMessage );
 router.get("/subscribe",subscribeMessage );
 
